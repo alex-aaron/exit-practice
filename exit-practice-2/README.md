f1cc5bf8cbd090a64e9c88e09a37be7791f2987c
Alex Aaron <alexfrancisaaron@gmail.com>
2022-10-18 20:38:49 +0000

    added getMeetingRecursively to js file, added tests

--- exit-practice-2/README.md
+++ exit-practice-2/README.md
@@ -1,6 +1,8 @@
-### Exit Exam Practice 
+### Exit Exam Practice 2
 
 **Directions:** The prompts in this practice exam are designed to gauge your facility with
-_iteration_, _object and array access_, and _creating functions_, _implementing higher order functions_.
+_iteration_, _object and array access_, _creating functions_, _implementing higher order functions_, and _recursion_.
 
 **Problem 1:** Create a function called `filterMeetingsByMonth` that takes in two parameters - `array` and `month`. The parameter `array` represents an array of meeting objects; `month` represents a string of a month. This function should use the native filter method to return a new array of only the meeting objects that take place in the given month.
+
+**Problem 2:** Create a function called `getMeetingRecursively` that takes in three parameters - `array`, `agenda`, and `date`. The parameter `array` represents an array of meeting objects; `agenda` is a string of the meeting agenda; `date` is a string of the meeting data. This function should iterate through the input array recursively and return the meeting object that matches the input agenda and date. If no meeting object matches, the function should return "no meeting found matching that criteria".
--- test/exit-practice-2.spec.js
+++ test/exit-practice-2.spec.js
@@ -1,6 +1,38 @@
 
+
 describe("exit-practice-2", function(){
-    it('should be a test', function(){
-        assert.equal('test', 'test');
-    })
+    describe('#filterMeetingsByMonth', function(){
+        it('should return an array', function(){
+            assert.equal(Array.isArray(filterMeetingsByMonth(meetings)), true);
+        });
+        it('should use the native filter method', function(){
+            assert.equal(filterMeetingsByMonth.toString().includes('.filter('), true);
+        });
+        it('should return a correct array of meetings', function(){
+            let result = [{
+                agenda: "Maryland Trip",
+                date: "February 25, 2022",
+                meetingTime: "10:00AM",
+                participants: ["Kyle", "Stan", "Nicole"]
+              }];
+            assert.deepEqual(filterMeetingsByMonth(meetings, 'February'), result);
+        });
+    });
+    describe("#getMeetingRecursively", function(){
+        it('should return an object if a meeting is found', function(){
+            let result = {
+                agenda: "Grant Reporting Requirements",
+                date: "March 1, 2022",
+                meetingTime: "1:00PM",
+                participants: ["Kyle", "Vickie", "Sean"]
+              };
+            assert.deepEqual(getMeetingRecursively(meetings, 'Grant Reporting Requirements', 'March 1, 2022'), result);
+        });
+        it('should return a string if no meeting is found', function(){
+            assert.equal(getMeetingRecursively(meetings, 'Grant Reporting Requirements', 'March 1, 2025'), 'no meeting found matching that criteria');
+        });
+        it('should call itself recursively', function(){
+            assert.equal(getMeetingRecursively.toString().includes('return getMeetingRecursively('), true);
+        });
+    });
 });
